6d606a76d11d53948b5054ac22682d0e6553ea3a
Gabriel Delfino <gabrieldelfino28@gmail.com>
2026-05-15 13:02:13 -0300

    feat: added a dynamic update method on HardwareTemplate that adapts to any future Inheritance

--- src/main/java/br/gov/sp/fateczl/museu/service/IHardwareService.java
+++ src/main/java/br/gov/sp/fateczl/museu/service/IHardwareService.java
@@ -3,6 +3,7 @@ package br.gov.sp.fateczl.museu.service;
 import br.gov.sp.fateczl.museu.domain.entity.Hardware;
 import br.gov.sp.fateczl.museu.domain.entity.Imagem;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IHardwareService<T extends Hardware> {
@@ -15,4 +16,12 @@ public interface IHardwareService<T extends Hardware> {
     T searchById(Long id);
 
     List<T> getAll();
+
+    List<T> searchByModelo(String modelo);
+
+    List<T> searchByFabricante(String fabricante);
+
+    List<T> searchByDataLancamento(LocalDate dataLancamento);
+
+    List<T> searchByPais(String paisOrigem);
 }
