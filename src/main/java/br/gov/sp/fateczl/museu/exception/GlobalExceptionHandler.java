b0a41892ddeda4a97021e4337b69509a67ce4186
Gabriel Delfino <gabrieldeflino28@gmail.com>
2026-05-20 12:58:42 -0300

    feat: add two functionality to GlobalExceptionHandler

--- src/main/java/br/gov/sp/fateczl/museu/exception/GlobalExceptionHandler.java
+++ src/main/java/br/gov/sp/fateczl/museu/exception/GlobalExceptionHandler.java
@@ -1,9 +1,22 @@
 package br.gov.sp.fateczl.museu.exception;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    
+    @ExceptionHandler(BusinessRuleException.class)
+    public String handleBusinessRule(BusinessRuleException ex, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("err", ex.getMessage());
+        return "redirect:/erro/negocio";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneral(Exception ex, Model model) {
+        model.addAttribute("mensagem", ex.getMessage());
+        return "error/500";
+    }
 }
