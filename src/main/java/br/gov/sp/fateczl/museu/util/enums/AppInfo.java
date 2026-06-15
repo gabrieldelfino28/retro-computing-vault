7a21d9638ef398c3b85e046c4d9806c0a2707059
Gabriel Delfino <gabrieldelfino28@gmail.com>
2026-06-15 11:15:00 -0300

    refactor + feat: refact current views + new styles
    Modified the current HTML views to get closer to official FATEC styling, and created a new html to handle 404 errors.

--- src/main/java/br/gov/sp/fateczl/museu/util/enums/AppInfo.java
+++ src/main/java/br/gov/sp/fateczl/museu/util/enums/AppInfo.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 @Getter
 public enum AppInfo {
 
-    DEFAULT_OS("Sistema Operacional Embutido / ROM Resident."),
-    DEFAULT_ROM("Configuração padrão de fábrica (ROM)."),
-    NOT_INFORMED("Informação não fornecida."),
-    UNKNOWN_VENDOR("Fabricante desconhecido."),
-    UNKNOWN_ORIGIN("País de origem desconhecido."),
-    UNKNOWN_LINE("Linha de Produto desconhecida.")
+    DEFAULT_OS("Sistema Operacional Embutido / ROM Resident"),
+    DEFAULT_ROM("Configuração padrão de fábrica (ROM)"),
+    NOT_INFORMED("Informação não fornecida"),
+    UNKNOWN_VENDOR("Fabricante desconhecido"),
+    UNKNOWN_ORIGIN("País de origem desconhecido"),
+    UNKNOWN_LINE("Linha de Produto desconhecida")
     ;
 
     private final String info;
